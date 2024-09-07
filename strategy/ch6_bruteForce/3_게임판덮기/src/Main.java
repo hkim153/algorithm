b28e841cf04ed64a5053c8aa240a0fdb96c18e24
hyunsoo <harry100429@hanmail.net>
2024-09-07 19:34:46 +0900

    [strategy][ch6] 3_게임판덮기 -my solution 수정

--- strategy/ch6_bruteForce/3_게임판덮기/src/Main.java
+++ strategy/ch6_bruteForce/3_게임판덮기/src/Main.java
@@ -3,8 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+/**
+ * <a href="https://algospot.com/judge/problem/read/BOARDCOVER">링크</a>
+ * <p>
+ * 문제
+ * 03.png
+ * <p>
+ * H*W 크기의 게임판이 있습니다.
+ * 게임판은 검은 칸과 흰 칸으로 구성된 격자 모양을 하고 있는데 이 중 모든 흰 칸을 3칸짜리 L자 모양의 블록으로 덮고 싶습니다.
+ * 이 때 블록들은 자유롭게 회전해서 놓을 수 있지만, 서로 겹치거나, 검은 칸을 덮거나, 게임판 밖으로 나가서는 안 됩니다.
+ * 위 그림은 한 게임판과 이를 덮는 방법을 보여줍니다.
+ * <p>
+ * 게임판이 주어질 때 이를 덮는 방법의 수를 계산하는 프로그램을 작성하세요.
+ * <p>
+ * 입력
+ * 력의 첫 줄에는 테스트 케이스의 수 C (C <= 30) 가 주어집니다.
+ * 각 테스트 케이스의 첫 줄에는 2개의 정수 H, W (1 <= H,W <= 20) 가 주어집니다.
+ * 다음 H 줄에 각 W 글자로 게임판의 모양이 주어집니다. # 은 검은 칸, . 는 흰 칸을 나타냅니다.
+ * 입력에 주어지는 게임판에 있는 흰 칸의 수는 50 을 넘지 않습니다.
+ * <p>
+ * 출력
+ * 한 줄에 하나씩 흰 칸을 모두 덮는 방법의 수를 출력합니다.
+ */
 /*
-입력:
+예제 입력
 3
 3 7
 #.....#
@@ -24,7 +46,7 @@ import java.util.StringTokenizer;
 #........#
 ##########
 
-답:
+예제 출력
 0
 2
 1514
@@ -42,7 +64,7 @@ public class Main
     {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
-        for(int i = 0; i < testCase; i++)
+        while(testCase-- > 0)
         {
             StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
             int ylen = Integer.parseInt(stringTokenizer.nextToken());
@@ -61,19 +83,20 @@ public class Main
                     map[y][x] = ch;
                 }
             }
-            System.out.println(possibleCovering(ylen, xlen, empty, map));
+            System.out.println(mySolution(empty, map));
         }
     }
 
-    private static int possibleCovering(int y, int x, int empty, char[][] map)
+    //책 solution이랑 같다.
+    private static int mySolution(int empty, char[][] map)
     {
         if(empty % 3 != 0)
             return 0;
 
-        return cover(map);
+        return mySolution(map);
     }
 
-    private static int cover(char[][] map)
+    private static int mySolution(char[][] map)
     {
         int cy = -1;
         int cx = -1;
@@ -99,7 +122,7 @@ public class Main
             if(check(cy, cx, type, map))
             {
                 set(cy, cx, type, map, '#');
-                ret += cover(map);
+                ret += mySolution(map);
                 set(cy, cx, type, map, '.');
             }
         }
